c7e6be5e306f19260e47b5508dad59adb7eddbc8
masgeek <barsamms@gmail.com>
2026-07-17 12:19:31 +0300

    🔧fix(DapperMapper): remove redundant mapping for `description`
    * Eliminated the mapping for `description` as it is already covered by `description_json`.
    * This change simplifies the type mapping logic in the DapperMapper class.

--- src/Data/DapperMapper.cs
+++ src/Data/DapperMapper.cs
@@ -20,7 +20,6 @@ public static class DapperMapper
                     "amount" => nameof(SmsNotification.Amount),
                     "receipt_no" => nameof(SmsNotification.ReceiptNo),
                     "dated" => nameof(SmsNotification.Dated),
-                    "description" => nameof(SmsNotification.Description),
                     "description_json" => nameof(SmsNotification.Description),
                     "status" => nameof(SmsNotification.Status),
                     "max_retries" => nameof(SmsNotification.MaxRetries),
